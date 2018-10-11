5f38f81dfb14ae360612bd83a699e35f47f1d39c
Luis Piedrahita <luis@lmseo.com>
2018-10-10 18:37:23 -0700

    Remove services references in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { SourceComponent } from './pages/work/source/source.component';
-import {AuthGuardService} from './auth-guard.service';
 import {WorkModule} from './pages/work/work.module';
-import {AuthService} from './auth/auth.service';
 import {AuthModule} from './auth/auth.module';
 import {CoreModule} from './core/core.module';
 
