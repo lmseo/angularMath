648683c4eb1d3702385fb145564914703aa4ed07
Luis Piedrahita <luis@lmseo.com>
2018-10-22 17:17:21 -0700

    Firebase hosting setup added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC0ZqrV5iX93dgHRZUU9H6X1sRzzY8_rYE',
+    authDomain: 'angualar-math.firebaseapp.com',
+  }
 };
